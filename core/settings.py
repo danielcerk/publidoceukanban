28e217f853ed06636b0d292aff8932bf43d7d290
Daniel Gomes <76263577+danielcerk@users.noreply.github.com>
2025-09-12 11:53:07 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -113,6 +113,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     r"^https:\/\/[a-z0-9\-]+\.ngrok-free\.app$",
+    "https://publidoceu.vercel.app/"
 
 ]
 
@@ -122,6 +123,7 @@ CSRF_TRUSTED_ORIGINS =  [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     r"^https:\/\/[a-z0-9\-]+\.ngrok-free\.app$",
+    "https://publidoceu.vercel.app/"
 
 ]
 
